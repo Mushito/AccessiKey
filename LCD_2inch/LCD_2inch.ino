a86af646c1431e435499217751f926250f75ddb8
Will Anderson <143653080+Mushito@users.noreply.github.com>
2024-10-29 22:56:44 -0600

    Update LCD_2inch.ino
    A-G

--- LCD_2inch/LCD_2inch.ino
+++ LCD_2inch/LCD_2inch.ino
@@ -8,28 +8,38 @@ void setup()
   Config_Init();
   LCD_Init();
   LCD_Clear(0xffff);
-  Paint_NewImage(LCD_HEIGHT, LCD_WIDTH, 90, WHITE);  // Width and height swapped
+  Paint_NewImage(LCD_WIDTH, LCD_HEIGHT, 90, WHITE);
   Paint_Clear(WHITE);
-  Paint_DrawCircle(160, 120, 40, BLACK, 2, 0);  // Swapped x and y values
 
-  Paint_DrawLine(200, 120, 220, 120, BLACK, 2, LINE_STYLE_SOLID);  // Top line
-  Paint_DrawLine(120, 120, 100, 120, BLACK, 2, LINE_STYLE_SOLID);  // Bottom line
-  Paint_DrawLine(160, 80, 160, 60, BLACK, 2, LINE_STYLE_SOLID);    // Left line
-  Paint_DrawLine(160, 160, 160, 180, BLACK, 2, LINE_STYLE_SOLID);  // Right line
+  // Apply horizontal mirroring to correct the flipped letters
+  Paint_SetMirroring(MIRROR_HORIZONTAL);
 
-  Paint_DrawLine(130, 90, 110, 70, BLACK, 2, LINE_STYLE_SOLID);    // Top-left diagonal
-  Paint_DrawLine(130, 150, 110, 170, BLACK, 2, LINE_STYLE_SOLID);  // Top-right diagonal
-  Paint_DrawLine(190, 90, 210, 70, BLACK, 2, LINE_STYLE_SOLID);    // Bottom-left diagonal
-  Paint_DrawLine(190, 150, 210, 170, BLACK, 2, LINE_STYLE_SOLID);  // Bottom-right diagonal
+  Paint_DrawCircle(160, 120, 40, BLACK, 2, 0);
 
-  Paint_DrawString_EN(210, 120, "A", &Font20, WHITE, BLACK);  // Swapped x and y values
+  // Vertical and horizontal lines
+  Paint_DrawLine(160, 160, 160, 180, BLACK, 2, LINE_STYLE_SOLID);
+  Paint_DrawLine(160, 80, 160, 60, BLACK, 2, LINE_STYLE_SOLID);    
+  Paint_DrawLine(200, 120, 220, 120, BLACK, 2, LINE_STYLE_SOLID);
+  Paint_DrawLine(120, 120, 100, 120, BLACK, 2, LINE_STYLE_SOLID);
+
+  // Diagonal lines
+  Paint_DrawLine(130, 150, 110, 170, BLACK, 2, LINE_STYLE_SOLID);
+  Paint_DrawLine(130, 90, 110, 70, BLACK, 2, LINE_STYLE_SOLID);
+  Paint_DrawLine(190, 150, 210, 170, BLACK, 2, LINE_STYLE_SOLID);
+  Paint_DrawLine(190, 90, 210, 70, BLACK, 2, LINE_STYLE_SOLID);
+
+  // Add letters A-G to lines
+  Paint_DrawString_EN(140, 190, "BASE", &Font20, WHITE, BLACK);
+  Paint_DrawString_EN(155, 40, "D", &Font20, WHITE, BLACK);
+  Paint_DrawString_EN(225, 115, "F", &Font20, WHITE, BLACK);
+  Paint_DrawString_EN(75, 115, "B", &Font20, WHITE, BLACK);
+  Paint_DrawString_EN(100, 170, "A", &Font20, WHITE, BLACK);
+  Paint_DrawString_EN(95, 55, "C", &Font20, WHITE, BLACK); 
+  Paint_DrawString_EN(210, 170, "G", &Font20, WHITE, BLACK);       
+  Paint_DrawString_EN(210, 60, "E", &Font20, WHITE, BLACK);        
 }
 
 void loop()
 {
   
 }
-
-/*********************************************************************************************************
-  END FILE
-*********************************************************************************************************/
